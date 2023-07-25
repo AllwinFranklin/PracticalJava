eab6e9a1beafeaa7c393569d9b850236867b3038
AllwinFranklin <allwinanto360@gmail.com>
2023-07-25 07:56:54 +0530

    Implemented Comparable interface

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
+import java.lang.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Init test");
+//        GenericHolder<User> genericHolder = new GenericHolder<User>();
+//        genericHolder.printer(new User("Allwin Anto Franklin", "allwinanto360@gmail.com"));
+
+        GenericHolder<Integer> intGenericHolder = new GenericHolder<>();
+        intGenericHolder.printer(1);
+
+        GenericHolder<Float> floatGenericHolder = new GenericHolder<>();
+        floatGenericHolder.printer(1.324F);
+
+        User employee1 = new User("Allwin", "allwin@gmail.com");
+        User employee2 = new User("Franklin", "franklin@gmail.com");
+        if (employee1.compareTo(employee2) < 0)
+            System.out.println(employee1);
+        else
+            System.out.println(employee2);
+    }
+}
+
+class GenericHolder<T extends Number> {
+    void printer(T Obj) {
+        System.out.println(Obj);
+    }
+}
+
+class User implements Comparable<User> {
+    String name;
+    String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(User other) {
+        return name.compareTo(other.name);
     }
 }
