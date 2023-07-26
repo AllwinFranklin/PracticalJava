d1f7f24ad31f38f0bf726bfc9d0a9254bb2abefb
AllwinFranklin <allwinanto360@gmail.com>
2023-07-26 08:57:20 +0530

    Splitted Generic from main.java to Generics package

---
+++ src/Generics/Generic.java
@@ -0,0 +1,50 @@
+package Generics;
+
+public class Generic {
+    public static void run() {
+//        GenericHolder<User> genericHolder = new GenericHolder<User>();
+//        genericHolder.printer(new User("Allwin Anto Franklin", "allwinanto360@gmail.com"));
+
+        GenericHolder<Integer> intGenericHolder = new GenericHolder<>();
+        intGenericHolder.printer(1);
+
+        GenericHolder<Float> floatGenericHolder = new GenericHolder<>();
+        floatGenericHolder.printer(1.324F);
+
+        User employee1 = new User("Allwin", "allwin@gmail.com");
+        User employee2 = new User("Franklin", "franklin@gmail.com");
+        if (employee1.compareTo(employee2) < 0)
+            System.out.println(employee1);
+        else
+            System.out.println(employee2);
+    }
+}
+
+class GenericHolder<T extends Number> {
+    void printer(T Obj) {
+        System.out.println(Obj);
+    }
+}
+
+class User implements Comparable<User> {
+    String name;
+    String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(User other) {
+        return name.compareTo(other.name);
+    }
+}
